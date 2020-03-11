fe4909b4081645575af99277e66ae85c62a47459
Mitch <mitchellnally@msn.com>
2020-03-12 09:51:35 +1100

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+    <h1>hello world</h1>
+    <?php
+    echo 'hello world';
+    ?>
+</body>
+
+</html>
